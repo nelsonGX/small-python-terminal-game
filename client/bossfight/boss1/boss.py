16d64bde8ee66dffe06095ec4f90f6a9870cef43
nelsonGX <hi@nelsongx.com>
2025-01-01 08:57:41 +0800

    change text

--- client/bossfight/boss1/boss.py
+++ client/bossfight/boss1/boss.py
@@ -2,6 +2,10 @@ import asyncio
 
 from client.bossfight.boss1.diceroller import Dice3D
 from client.assets.reader import get_gui
+from server import Server
+
+async def get_boss_data():
+    pass # TODO @andy
 
 async def fight():
     # start the game
